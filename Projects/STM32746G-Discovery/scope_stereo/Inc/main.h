47d42eb64b6b917c62a93cf1511c6a891b22ac83
vpecanins <vpecanins@.gmail.com>
2016-12-27 16:04:06 +0100

    Scope initial commit BUTTON_MODE_EXTI

---
+++ Projects/STM32746G-Discovery/scope_stereo/Inc/main.h
@@ -0,0 +1,127 @@
+/**
+  ******************************************************************************
+  * @file    BSP/Inc/main.h 
+  * @author  MCD Application Team
+  * @version V1.0.3
+  * @date    22-April-2016 
+  * @brief   Header for main.c module
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT(c) 2016 STMicroelectronics</center></h2>
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+  
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+/* Includes ------------------------------------------------------------------*/
+#include "stdio.h"
+#include "stm32746g_discovery.h"
+#include "stm32746g_discovery_ts.h"
+#include "stm32746g_discovery_lcd.h"
+#include "stm32746g_discovery_sdram.h"
+#include "stm32746g_discovery_audio.h"
+#include "stm32746g_discovery_sd.h"
+#include "lcd_log.h"
+
+/* Macros --------------------------------------------------------------------*/
+#ifdef USE_FULL_ASSERT
+/* Assert activated */
+#define ASSERT(__condition__)                do { if(__condition__) \
+                                                   {  assert_failed(__FILE__, __LINE__); \
+                                                      while(1);  \
+                                                    } \
+                                              }while(0)
+#else
+/* Assert not activated : macro has no effect */
+#define ASSERT(__condition__)                  do { if(__condition__) \
+                                                   {  ErrorCounter++; \
+                                                    } \
+                                              }while(0)
+#endif /* USE_FULL_ASSERT */
+
+#define RGB565_BYTE_PER_PIXEL     2
+#define ARBG8888_BYTE_PER_PIXEL   4
+
+
+/**
+  * @brief  LCD FB_StartAddress
+  * LCD Frame buffer 0 start address : starts at beginning of SDRAM
+  * Assuming LCD frame buffer is of size 480x800 and format ARGB8888 (32 bits per pixel).
+  */
+#define LCD_FB_LENGTH               (RK043FN48H_WIDTH * RK043FN48H_HEIGHT * ARBG8888_BYTE_PER_PIXEL)
+#define LCD_FB0_START_ADDRESS       SDRAM_DEVICE_ADDR
+#define LCD_FB1_START_ADDRESS       ((uint32_t)(LCD_FB0_START_ADDRESS + LCD_FB_LENGTH))
+#define LCD_FB2_START_ADDRESS       ((uint32_t)(LCD_FB1_START_ADDRESS + LCD_FB_LENGTH))
+#define LCD_FB3_START_ADDRESS       ((uint32_t)(LCD_FB2_START_ADDRESS + LCD_FB_LENGTH))
+/**
+  * @brief  Audio buffer next to LCD frame buffers.
+  */
+#define SDRAM_WRITE_READ_ADDR       ((uint32_t)(LCD_FB3_START_ADDRESS + LCD_FB_LENGTH))
+#define AUDIO_BLOCK_SIZE    		((uint32_t)0x10000-2) /* in WORDS (16-bit values) */
+
+#define AUDIO_REC_START_ADDR_L      (SDRAM_WRITE_READ_ADDR)
+#define AUDIO_REC_START_ADDR_R      (AUDIO_REC_START_ADDR_L+0x10000)
+
+/* Exported types ------------------------------------------------------------*/
+typedef struct
+{
+  void   (*DemoFunc)(void);
+  uint8_t DemoName[50]; 
+  uint32_t DemoIndex;
+}BSP_DemoTypedef;
+
+typedef enum {
+  AUDIO_ERROR_NONE = 0,
+  AUDIO_ERROR_NOTREADY,
+  AUDIO_ERROR_IO,
+  AUDIO_ERROR_EOF,
+}AUDIO_ErrorTypeDef;
+
+extern const unsigned char stlogo[];
+/* Exported variables ---------------------------------------------------*/
+extern uint8_t     NbLoop;
+extern uint8_t     MfxExtiReceived;
+#ifndef USE_FULL_ASSERT
+extern uint32_t    ErrorCounter;
+#endif
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions ------------------------------------------------------- */
+
+uint8_t AUDIO_Process(void);
+
+uint8_t CheckForUserInput(void);
+void Toggle_Leds(void);
+void BSP_LCD_DMA2D_IRQHandler(void);
+#ifdef USE_FULL_ASSERT
+void assert_failed(uint8_t* file, uint32_t line);
+#endif
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
